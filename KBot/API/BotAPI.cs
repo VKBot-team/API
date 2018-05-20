b85a7f3076217183d1b5fed2e05a1457fbc2ffd7
lenastar <lena.starkova.1998@mail.ru>
2018-05-20 19:48:52 +0300

    Created API Classes

---
+++ KBot/API/BotAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using KBot.API;
+
+
+namespace KBot
+{
+    class BotAPI:IBotAPI
+    {
+        private static readonly Dictionary<string, Action<string[]>> Commands
+            = new Dictionary<string, Action<string[]>>();
+
+        public void ExecuteCommand(string[] args)
+        {
+            var command = args[0];
+            if (!Commands.ContainsKey(command))
+            {
+                throw new ArgumentException("Command is not found");
+            }
+            var list = args.Skip(1).ToArray();
+            Commands[command](list);
+        }
+    }
+}
+
---
+++ KBot/API/BotAPI_should.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace KBot.API
+{
+    [TestFixture]
+    class BotAPI_should
+    {
+        [Test]
+        public void TestInvalid()
+        {
+            Assert.Catch<ArgumentException>(()=>new BotAPI().ExecuteCommand(new[]{"lol"}));
+        }
+
+    }
+
+    
+}
