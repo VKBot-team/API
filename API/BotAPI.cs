3f578883ae5e9fb526055ff3dc0fb02007f00f92
TAB0R <vlad.eganov@gmail.com>
2018-05-27 20:00:56 +0500

    add commands: help, gg

--- API/BotAPI.cs
+++ API/BotAPI.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VKApiCore;
+using GIFService;
 
 namespace API
 {
@@ -12,35 +12,59 @@ namespace API
         private static readonly Dictionary<string, Func<string[], Task>> Commands
             = new Dictionary<string, Func<string[], Task>>();
 
-        private static readonly VkService vks;
+        private static readonly VkService vkService;
+        private static readonly GifService gifService;
 
         static BotAPI()
         {
-            vks = new VkService();
+            vkService = new VkService();
+            gifService = new GifService();
 
             Commands["call"] = async args =>
             {
-                var users = await vks.GetGroupUsers();
-
-                foreach (var user in users)
-                {
-                    await vks.SendMessageById(user, args[0]);
-                }
+                var users = await vkService.GetGroupUsers();
+                await vkService.SendMessagesByIds(users, args[0]);
             };
 
             Commands["ok"] = async args =>
             {
-                await vks.SendMessageById(args[0], "Ok. Request is processing");
+                await vkService.SendMessageById(args[0], "Ok. Request is processing");
             };
 
             Commands["error"] = async args =>
             {
-                await vks.SendMessageById(args[0], "Command not found");
+                await vkService.SendMessageById(args[0], "Command not found. Send help");
             };
 
             Commands["anime"] = async args =>
             {
-                await vks.SendMessageById(args[0], AnimeList.Urls[new Random().Next(0, AnimeList.Urls.Count)]);
+                await vkService.SendMessageById(args[0], AnimeList.Urls[new Random().Next(0, AnimeList.Urls.Count)]);
+            };
+
+            Commands["help"] = async args =>
+            {
+                await vkService.SendMessageById(args[0], HelpText.Text);
+            };
+
+            Commands["gg"] = async args =>
+            {
+                string userId;
+                string gifLink;
+                if (args.Length > 1)
+                {
+                    userId = args[1];
+                    gifLink = gifService.GetGifByCategory(args[0]);
+                }
+                else
+                {
+                    userId = args[0];
+                    gifLink = gifService.GetRandomGif();
+                }
+
+                if (gifLink == null)
+                    await Commands["help"](new[] { userId });
+                else
+                    await vkService.SendGifById(userId, gifLink);
             };
         }
 
---
+++ API/HelpText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API
+{
+    public static class HelpText
+    {
+        public static string Text = "Commands: call \"message\" - notify all group members. " +
+            "gg - get random gif. gg [pet|kb] - get gif by genre. " +
+            "anime - get random anime.";
+    }
+}
